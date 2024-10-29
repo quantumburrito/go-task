f1eafbce3098894752497cf9eb64d4671bcf65d4
quantumburrito <107977173+quantumburrito@users.noreply.github.com>
2024-10-29 16:57:53 -0500

    making test pass

--- file_task_repository_test.go
+++ file_task_repository_test.go
@@ -203,7 +203,7 @@ func TestDescribeTasks_TaskRepository(t *testing.T) {
 	t.Run("Attempt to list only Tasks with ToDo Status", func(t *testing.T) {
 		// Something like tl.DescribeTasks("ToDo")
 
-		tl, _ := createStructuredFileTaskRepositoryAndTaskIDSlice(t, 10)
+		// tl, _ := createStructuredFileTaskRepositoryAndTaskIDSlice(t, 10)
 
 	})
 
